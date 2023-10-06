5f75958bfa3614f82ca24bafdf585e2c9a6075aa
deltakroneker <nikola.milic.ios@gmail.com>
2023-10-06 09:12:04 +0200

    Add Movie and Genre domain entities

---
+++ TMDBDemo/Domain/Genre.swift
@@ -0,0 +1,13 @@
+//
+//  Genre.swift
+//  TMDBDemo
+//
+//  Created by nikolamilic on 10/6/23.
+//
+
+import Foundation
+
+struct Genre {
+    let id: Int
+    let name: String
+}
---
+++ TMDBDemo/Domain/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  TMDBDemo
+//
+//  Created by nikolamilic on 10/6/23.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let name: String
+    let genres: [Genre]
+    let poster: String
+    let rating: Double
+}
