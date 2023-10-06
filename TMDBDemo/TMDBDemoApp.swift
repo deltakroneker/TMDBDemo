008fcf9d80d86a6d739ec518ac4ab532c932205b
deltakroneker <nikola.milic.ios@gmail.com>
2023-10-06 08:38:53 +0200

    Initial Commit

---
+++ TMDBDemo/TMDBDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBDemoApp.swift
+//  TMDBDemo
+//
+//  Created by nikolamilic on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDBDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
