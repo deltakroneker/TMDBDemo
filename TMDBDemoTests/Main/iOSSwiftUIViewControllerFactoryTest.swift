8972a4af99eb57d7b96491ae6b287f3f0853ac42
deltakroneker <nikola.milic.ios@gmail.com>
2023-10-09 16:44:44 +0200

    Add rest of the iOSSwiftUIViewControllerFactoryTest screen tests

--- TMDBDemoTests/Main/iOSSwiftUIViewControllerFactoryTest.swift
+++ TMDBDemoTests/Main/iOSSwiftUIViewControllerFactoryTest.swift
@@ -18,7 +18,30 @@ final class iOSSwiftUIViewControllerFactoryTest: XCTestCase {
         XCTAssertNotNil(controller)
     }
     
+    @MainActor func test_movieDetailsScreen_createsControllerWithMovieDetailsViewAsRootView() {
+        let sut = makeSUT()
+        let controller = sut.movieDetailsScreen(movie: self.dummyMovie) as? UIHostingController<MovieDetailsView>
+        
+        XCTAssertNotNil(controller)
+    }
+    
+    @MainActor func test_favoriteMoviesScreen_createsControllerWithFavoriteMoviesViewAsRootView() {
+        let sut = makeSUT()
+        let controller = sut.favoriteMoviesScreen(movieTapAction: { _ in }) as? UIHostingController<FavoriteMoviesView>
+        
+        XCTAssertNotNil(controller)
+    }
+    
+    @MainActor func test_movieSearchScreen_createsControllerWithMovieSearchViewAsRootView() async {
+        let sut = makeSUT()
+        let controller = sut.movieSearchScreen(movieTapAction: { _ in }) as? UIHostingController<MovieSearchView>
+        
+        XCTAssertNotNil(controller)
+    }
+    
     // Helpers:
+    
+    private let dummyMovie = Movie(id: 0, name: "Inception", genres: [Genre(id: 3, name: "Action"), Genre(id: 4, name: "Sci-Fi")], poster: "/inception_poster.jpg", rating: 8.8, description: "Test")
     
     private func makeSUT() -> iOSSwiftUIViewControllerFactory {
         return iOSSwiftUIViewControllerFactory(genresService: GenresService(loader: DummyGenreLoader()))
